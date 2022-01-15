b98e4380f2f07978fedc03a19de7a116984f15f5
Christopher Ward <cdurianward@gmail.com>
2022-01-15 01:00:48 -0800

    Add consent page

---
+++ app/javascript/components/app/ConsentPage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+
+const ConsentPage = ({ setConsent }) => {
+  return (
+    <div>
+      <p className="consent-page">Insert consent text here.</p>
+      <div className="consent-buttons">
+        <Button
+          id="consent"
+          onClick={() => {
+            setConsent(true);
+          }}
+        >
+          I consent to this!
+        </Button>
+        <Button
+          id="leave"
+          onClick={() => {
+            window.location.href = 'about:blank';
+          }}
+        >
+          Get me out of here!
+        </Button>
+      </div>
+
+      <div className="credits">
+        <h1 className="credit-title">Credits</h1>
+        <p className="credit">Designed by Caitlyn Kilgore and Chris Ward</p>
+        <p className="credit">Created by Chris Ward</p>
+        <p className="credit">
+          &quot;Bell, Counter, A.wav&quot; by InspectorJ (www.jshaw.co.uk) of Freesound.org
+        </p>
+      </div>
+    </div>
+  );
+};
+
+ConsentPage.propTypes = {
+  setConsent: PropTypes.func.isRequired,
+};
+
+export default ConsentPage;
