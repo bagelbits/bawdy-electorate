cb54a88b9ffaec02443341138b71282d9d2465cf
Christopher Ward <cdurianward@gmail.com>
2022-01-24 11:48:17 -0800

    Fix test

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -2,11 +2,12 @@ describe WordsController, type: :controller do
   describe 'GET #index' do
     it 'returns all words' do
       create_list(:word, 3)
+      extra_word = build(:word, word: Word::WORD_OF_THE_DAY, count: Word.maximum(:count) * 2)
 
       get :index
 
       expect(response).to have_http_status(:ok)
-      expect(response.body).to eq(Word.all.map(&:to_h).to_json)
+      expect(response.body).to eq((Word.all.map(&:to_h).to_a << extra_word.to_h).to_json)
     end
   end
 
